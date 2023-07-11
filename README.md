f0c1a16572cb81d6087f680abe62bed94c56edee
Sumant <sumantrane52@gmail.com>
2023-07-11 15:09:16 +0530

    episode-3

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 #Nameste React🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File watching algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of file
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - to support older browsers
+- Diagnostics
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code for us
--- app.js
+++ app.js
@@ -1,16 +1,64 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a h1 tag from react"),
-    React.createElement("h2", {}, "I am a h3 tag from react"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag from react"),
-    React.createElement("h2", {}, "I am a h3 tag from react"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent);
+//React.createElement => Object => HTMLElement(render)
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Welcome to React. 🚀"
+);
+
+//jsx - its not html in javascript its html/XML like syntax.
+//jsx=>Babel transpiles it to (React.createElement) => ReactElement (js Object) => HTMLElement(render)
+
+const number = 10000;
+
+//React Component
+const HeadingComponent = () =>
+(
+  <>
+    <div className="container">
+      <Title />
+      <Title></Title>
+      {Title()}
+      {jsxHeading}
+      <h2>{Math.floor(number / 3)}</h2>
+      <h1>This is functional component heading...🚀</h1>
+    </div>
+    <div className="container-2">
+      <h3>This is container 2</h3>
+    </div>
+  </>
+)
+
+const NavbarComponent = () => (
+  <div className="container">
+    <a href="#">Home</a>
+    <a href="#">About us</a>
+    <a href="#">Contact us</a>
+  </div>
+)
+
+const Title = () =>
+(
+  <div>
+    <h3 className="title">This is title component</h3>
+  </div>
+)
+
+//React element
+const jsxHeading = (
+  <h1 id="jsxHeading" className="header" tabIndex='1'>
+    This is jsx heading 🚀
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+
+//root.render(heading);
+//root.render(jsxHeading);
+root.render(<HeadingComponent />)
+
+root2.render(<NavbarComponent />)
